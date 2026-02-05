3dd0ffd073dda3da85f525b43a55b02fb2c5fd45
Niponpal <niponpal52@gmail.com>
2026-02-06 00:25:19 +0600

    Add project files.

---
+++ EventsMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventsMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
