986767a3a05737b07e9f7f46b9d60c46e80735c8
Niponpal <niponpal52@gmail.com>
2026-02-08 10:50:54 +0600

    Fix  ICategory 🚀🚀🚑

---
+++ EventsMS/Repository/ICategory.cs
@@ -0,0 +1,12 @@
+﻿using EventsMS.Models;
+
+namespace EventsMS.Repository;
+
+public interface ICategory
+{
+    Task<IEnumerable<Category>> GetAllCategoryAsync(CancellationToken cancellationToken);
+    Task<Category?> GetCategoryByIdAsync(long id, CancellationToken cancellationToken);
+    Task<Category> AddCategoryAsync(Category  category, CancellationToken cancellationToken);
+    Task<Category?> UpdateCategoryAsync(Category  category, CancellationToken cancellationToken);
+    Task<Category> DeleteCategoryAsync(long id, CancellationToken cancellationToken);
+}
