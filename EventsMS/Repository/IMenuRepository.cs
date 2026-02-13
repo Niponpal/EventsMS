35078844b6b0a75e0daf8194770fc504bb58f723
Niponpal <niponpal52@gmail.com>
2026-02-13 11:56:21 +0600

    Fix IMenuRepository  🚀🚀🚑

---
+++ EventsMS/Repository/IMenuRepository.cs
@@ -0,0 +1,13 @@
+﻿using EventsMS.Models;
+
+namespace EventsMS.Repository
+{
+    public interface IMenuRepository
+    {
+        Task<IEnumerable<Menu>> GetAllMenuAsync(CancellationToken cancellationToken);
+        Task<Menu?> GetMenuByIdAsync(long id, CancellationToken cancellationToken);
+        Task<Menu> AddMenuAsync(Menu  menu, CancellationToken cancellationToken);
+        Task<Menu?> UpdateMenuAsync(Menu  menu, CancellationToken cancellationToken);
+        Task<Menu> DeleteMenuAsync(long id, CancellationToken cancellationToken);
+    }
+}
