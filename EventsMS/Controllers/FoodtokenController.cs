15de8dc2a252547e3c9502b67d7f7a3315e61a07
Niponpal <niponpal52@gmail.com>
2026-02-14 13:04:26 +0600

    Fix Foodtoken Index 🚀🚀🚑

---
+++ EventsMS/Controllers/FoodtokenController.cs
@@ -0,0 +1,52 @@
+﻿using EventsMS.Models;
+using EventsMS.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EventsMS.Controllers;
+
+public class FoodtokenController : Controller
+{
+    private readonly IFoodTokenRepository _foodTokenRepository;
+
+    public FoodtokenController(IFoodTokenRepository foodTokenRepository)
+    {
+        _foodTokenRepository = foodTokenRepository;
+    }
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var data = await _foodTokenRepository.GetAllFoodTokenAsync(cancellationToken);
+        return View(data);
+    }
+    [HttpGet]
+    public async Task<IActionResult>CreateOrEdit(long id, CancellationToken cancellationToken)
+    {
+        if (id == 0)
+        {
+            return View(new Models.FoodToken());
+        }
+        else
+        {
+            var data = await _foodTokenRepository.GetFoodTokenByIdAsync(id, cancellationToken);
+            if (data != null)
+            {
+                return View(data);
+            }
+            return NotFound();
+        }
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateOrEdit(FoodToken foodToken, CancellationToken cancellationToken)
+    {
+        if (foodToken.Id == 0)
+            {
+                await _foodTokenRepository.AddFoodTokenAsync(foodToken, cancellationToken);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                await _foodTokenRepository.UpdateFoodTokenAsync(foodToken, cancellationToken);
+                return RedirectToAction(nameof(Index));
+            }     
+    }
+}
