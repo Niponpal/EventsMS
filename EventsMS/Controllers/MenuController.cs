275a69cb3762a97f225d0c1ec91beb049225d218
Niponpal <niponpal52@gmail.com>
2026-02-14 22:16:16 +0600

    Fix Menu 🚀🚀🚑

---
+++ EventsMS/Controllers/MenuController.cs
@@ -0,0 +1,73 @@
+﻿using EventsMS.Data;
+using EventsMS.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace EventsMS.Controllers;
+
+public class MenuController : Controller
+{
+    private readonly IMenuRepository _menuRepository;
+    
+    public MenuController(IMenuRepository menuRepository)
+    {
+        _menuRepository = menuRepository;
+    }
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var data = await _menuRepository.GetAllMenuAsync(cancellationToken);
+        if (data != null)
+        {
+            return View(data);
+        }   
+        return View(NotFound());
+    }
+    [HttpGet]   
+    public async Task<IActionResult> CreateOrEdit(long id, CancellationToken cancellationToken)
+    {
+        if (id == 0)
+        {
+            return View(new Models.Menu());
+        }
+        else
+        {
+            var data = await _menuRepository.GetMenuByIdAsync(id, cancellationToken);
+            if (data != null)
+            {
+                return View(data);
+            }
+            return NotFound();
+        }
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateOrEdit(Models.Menu menu, CancellationToken cancellationToken)
+    {
+        if (menu.Id == 0)
+        {
+           await _menuRepository.AddMenuAsync(menu, cancellationToken);
+            return RedirectToAction(nameof(Index));
+        }
+        else
+        {
+           await _menuRepository.UpdateMenuAsync(menu, cancellationToken);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+    [HttpPost]
+    public async Task<IActionResult> Delete(long id, CancellationToken cancellationToken)
+    {
+        await _menuRepository.DeleteMenuAsync(id, cancellationToken);
+        return RedirectToAction(nameof(Index));
+    }
+     [HttpGet]
+     public async Task<IActionResult> Details(long id, CancellationToken cancellationToken)
+    {
+        var data = await _menuRepository.GetMenuByIdAsync(id, cancellationToken);
+        if (data != null)
+        {
+            return View(data);
+        }
+        return NotFound();
+    }
+}
