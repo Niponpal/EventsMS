0bf96b0e34274d273b4af3ead5b4f3089b523e6a
Niponpal <niponpal52@gmail.com>
2026-02-15 18:46:02 +0600

    Fix PaymentHistoryRepository 🚀🚀🚑

---
+++ EventsMS/Repository/IPaymentHistoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using EventsMS.Models;
+
+namespace EventsMS.Repository;
+
+public interface IPaymentHistoryRepository
+{
+    Task<IEnumerable<PaymentHistory>> GetAllPaymentHistoryAsync(CancellationToken cancellationToken);
+    Task<PaymentHistory?> GetPaymentHistoryByIdAsync(long id, CancellationToken cancellationToken);
+    Task<PaymentHistory> AddPaymentHistoryAsync(PaymentHistory  paymentHistory, CancellationToken cancellationToken);
+    Task<PaymentHistory?> UpdatePaymentHistoryAsync(PaymentHistory  paymentHistory, CancellationToken cancellationToken);
+    Task<PaymentHistory> DeletePaymentHistoryAsync(long id, CancellationToken cancellationToken);
+}
---
+++ EventsMS/Repository/PaymentHistoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using EventsMS.Models;
+
+namespace EventsMS.Repository
+{
+    public class PaymentHistoryRepository : IPaymentHistoryRepository
+    {
+        public Task<PaymentHistory> AddPaymentHistoryAsync(PaymentHistory paymentHistory, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaymentHistory> DeletePaymentHistoryAsync(long id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PaymentHistory>> GetAllPaymentHistoryAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaymentHistory?> GetPaymentHistoryByIdAsync(long id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaymentHistory?> UpdatePaymentHistoryAsync(PaymentHistory paymentHistory, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
