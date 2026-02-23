6757b3573d8cd6ab12626272428f12efb686948f
Niponpal <niponpal52@gmail.com>
2026-02-23 12:12:53 +0600

    Fix RegisterViewModel 🚀🚀🚑

--- EventsMS/Program.cs
+++ EventsMS/Program.cs
@@ -1,6 +1,10 @@
+using EventsMS;
 using EventsMS.Data;
+using EventsMS.Helper;
 using EventsMS.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using static EventsMS.Auth_IdentityModel.IdentityModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +22,24 @@ builder.Services.AddScoped<IMenuRepository, MenuRepository>();
 builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
 builder.Services.AddScoped<IPaymentHistoryRepository, PaymentHistoryRepository>();
 builder.Services.AddScoped<IStudentRegistrationRepository, StudentRegistrationRepository>();
+
+
+// Add Identity with custom classes and long key
+builder.Services.AddIdentity<User, Role>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+
+builder.Services.AddTransient<ISignInHelper, SignInHelper>();
 
 
 var app = builder.Build();
