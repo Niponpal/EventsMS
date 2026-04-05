7641df30374cf14e7cea8201a173972c21295dba
Niponpal <niponpal52@gmail.com>
2026-04-05 15:10:52 +0600

    Fix Issue 🚀🚀🚑

--- EventsMS/Repository/IStudentRegistrationRepository.cs
+++ EventsMS/Repository/IStudentRegistrationRepository.cs
@@ -10,7 +10,7 @@ public interface IStudentRegistrationRepository
     Task<StudentRegistration> AddStudentRegistrationAsync(StudentRegistration  studentRegistration, CancellationToken cancellationToken);
     Task<StudentRegistration?> UpdateStudentRegistrationAsync(StudentRegistration  studentRegistration, CancellationToken cancellationToken);
     Task<StudentRegistration> DeleteStudentRegistrationAsync(long id, CancellationToken cancellationToken);
-    Task<List<StudentRegistration>> GetMyRegistrationsAsync(string email, CancellationToken cancellationToken);
     IEnumerable<SelectListItem> Dropdown();
+    Task<List<StudentRegistration>> GetMyRegistrationsAsync(string email, CancellationToken cancellationToken);
 
 }
